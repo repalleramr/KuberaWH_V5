9ac82fcc3855c545a8fbccf94347bcb6cd497b9a
repalleramr <repalleramr@gmail.com>
2026-03-27 19:27:10 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kubera-warhunt-v5pro-final-v10';
+const CACHE_NAME = 'kubera-warhunt-v5pro-fourthstrike-newshoe-undo21-final';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
